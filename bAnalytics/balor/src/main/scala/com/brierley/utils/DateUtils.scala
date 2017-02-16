2126fb81aa9ca32905d7aa1d250ba75f76a7d069
merrill-brier <amerrill@brierley.com>
2017-02-16 08:14:54 -0600

    Balor tests and method defs, few tweaks to cadence tests

---
+++ bAnalytics/balor/src/main/scala/com/brierley/utils/DateUtils.scala
@@ -0,0 +1,8 @@
+package com.brierley.utils
+
+/**
+  * Created by amerrill on 2/14/17.
+  */
+object DateUtils {
+
+}
---
+++ bAnalytics/balor/src/test/scala/com/brierley/balor/tests/DateUtilsTest.scala
@@ -0,0 +1,8 @@
+package com.brierley.balor.tests
+
+/**
+  * Created by amerrill on 2/14/17.
+  */
+class DateUtilsTest {
+
+}
