117585fd427212447a8b0c76ed3e6c547881fa68
merrill-brier <amerrill@brierley.com>
2017-02-21 09:57:00 -0600

    DateUtils methods fleshed out, along with unit tests.  Util methods should be called after knowing the date format, they do not determine format

--- bAnalytics/balor/src/main/scala/com/brierley/utils/DateUtils.scala
+++ bAnalytics/balor/src/main/scala/com/brierley/utils/DateUtils.scala
@@ -1,22 +1,35 @@
 package com.brierley.utils
 
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{unix_timestamp, to_date}
 
 /**
   * Created by amerrill on 2/14/17.
   */
 object DateUtils {
 
-  def convertDateDaySlash(initialDF: DataFrame): DataFrame = ???
+  def convertDateDaySlash(initialDF: DataFrame): DataFrame = {
+    initialDF.withColumn("Date", to_date(unix_timestamp(initialDF("TXN_DATE"), "dd/MM/yyyy").cast("timestamp")))
+  }
 
-  def convertDateDayDash(initialDF: DataFrame): DataFrame = ???
+  def convertDateDayDash(initialDF: DataFrame): DataFrame = {
+    initialDF.withColumn("Date", to_date(unix_timestamp(initialDF("TXN_DATE"), "dd-MM-yyyy").cast("timestamp")))
+  }
 
-  def convertDateMonthSlash(initialDF: DataFrame): DataFrame = ???
+  def convertDateMonthSlash(initialDF: DataFrame): DataFrame = {
+    initialDF.withColumn("Date", to_date(unix_timestamp(initialDF("TXN_DATE"), "MM/dd/yyyy").cast("timestamp")))
+  }
 
-  def convertDateMonthDash(initialDF: DataFrame): DataFrame = ???
+  def convertDateMonthDash(initialDF: DataFrame): DataFrame = {
+    initialDF.withColumn("Date", to_date(unix_timestamp(initialDF("TXN_DATE"), "MM-dd-yyyy").cast("timestamp")))
+  }
 
-  def convertDateYearSlash(initialDF: DataFrame): DataFrame = ???
+  def convertDateYearSlash(initialDF: DataFrame): DataFrame = {
+    initialDF.withColumn("Date", to_date(unix_timestamp(initialDF("TXN_DATE"), "yyyy/MM/dd").cast("timestamp")))
+  }
 
-  def convertDateYearDash(initialDF: DataFrame): DataFrame = ???
+  def convertDateYearDash(initialDF: DataFrame): DataFrame = {
+    initialDF.withColumn("Date", to_date(unix_timestamp(initialDF("TXN_DATE"), "yyyy-MM-dd").cast("timestamp")))
+  }
 
 }
--- bAnalytics/balor/src/test/scala/com/brierley/balor/tests/DateUtilsTest.scala
+++ bAnalytics/balor/src/test/scala/com/brierley/balor/tests/DateUtilsTest.scala
@@ -1,5 +1,7 @@
 package com.brierley.balor.tests
 
+import java.sql.Date
+
 import com.brierley.utils.DateUtils
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.junit.runner.RunWith
@@ -35,7 +37,7 @@ class DateUtilsTest extends FunSuite with DataFrameSuiteBase{
       ("customerA", "txn2-custA-Feb2", "22/02/2016", 22.10, 1, 2.35),
       ("customerA", "txn3-custA-Feb3", "29/02/2016", 10.50, 1, 2.35),
       ("customerA", "txn4-custA-Mar1", "2/3/2016", 25.45, 1, 2.35),
-      ("customerA", "txn5-custA-Mar2", "03/5/2016", 1.50, 1, 2.35),
+      ("customerA", "txn5-custA-Mar2", "5/03/2016", 1.50, 1, 2.35),
       ("customerA", "txn6-custA-Mar3", "12/3/2016", 3.25, 2, 2.13),
       ("customerA", "txn7-custA-Mar4", "13/03/2016", 4.36, 4, 1.24),
       ("customerA", "txn8-custA-Mar5", "14/03/2016", 3.42, 1, 1.21)
@@ -89,31 +91,75 @@ class DateUtilsTest extends FunSuite with DataFrameSuiteBase{
       ("customerA", "txn8-custA-Mar5", "03/14/2016", 3.42, 1, 1.21)
     )).toDF("CUST_ID", "TXN_ID", "TXN_DATE", "TXN_AMT", "ITEM_QTY", "DISC_AMT")
 
-    val dateList = List()
+    val dateinput = sc.parallelize(List(
+      ("customerA", "txn1-custA-Feb1", "2016-02-15", 12.34, 1, 2.35),
+      ("customerA", "txn2-custA-Feb2", "2016-02-22", 22.10, 1, 2.35),
+      ("customerA", "txn3-custA-Feb3", "2016-02-29", 10.50, 1, 2.35),
+      ("customerA", "txn4-custA-Mar1", "2016-03-02", 25.45, 1, 2.35),
+      ("customerA", "txn5-custA-Mar2", "2016-03-05", 1.50, 1, 2.35),
+      ("customerA", "txn6-custA-Mar3", "2016-03-12", 3.25, 2, 2.13),
+      ("customerA", "txn7-custA-Mar4", "2016-03-13", 4.36, 4, 1.24),
+      ("customerA", "txn8-custA-Mar5", "2016-03-14", 3.42, 1, 1.21)
+    )).toDF("CUST_ID", "TXN_ID", "TXN_DATE", "TXN_AMT", "ITEM_QTY", "DISC_AMT")
+
+    val dateResults = dateinput.withColumn("Date", dateinput("TXN_DATE").cast("date"))
+      .select("CUST_ID", "TXN_ID", "TXN_AMT", "ITEM_QTY", "DISC_AMT", "Date")
+
   }
 
   test("Date format of MM/dd/yyyy"){
 
+    new TransformDateData {
+      val trans = DateUtils.convertDateMonthSlash(leapSavingsDate6)
+        .select("CUST_ID", "TXN_ID", "TXN_AMT", "ITEM_QTY", "DISC_AMT", "Date")
+
+      assertDataFrameEquals(trans, dateResults)
+    }
   }
 
   test("Date format of MM-dd-yyyy"){
+    new TransformDateData {
+      val trans = DateUtils.convertDateMonthDash(leapSavingsDate5)
+        .select("CUST_ID", "TXN_ID", "TXN_AMT", "ITEM_QTY", "DISC_AMT", "Date")
 
+      assertDataFrameEquals(trans, dateResults)
+    }
   }
 
   test("Date format of dd-MM-yyyy"){
+    new TransformDateData {
+      val trans = DateUtils.convertDateDayDash(leapSavingsDate1)
+        .select("CUST_ID", "TXN_ID", "TXN_AMT", "ITEM_QTY", "DISC_AMT", "Date")
 
+      assertDataFrameEquals(trans, dateResults)
+    }
   }
 
   test("Date format of dd/MM/yyyy"){
+    new TransformDateData {
+      val trans = DateUtils.convertDateDaySlash(leapSavingsDate2)
+        .select("CUST_ID", "TXN_ID", "TXN_AMT", "ITEM_QTY", "DISC_AMT", "Date")
 
+      assertDataFrameEquals(trans, dateResults)
+    }
   }
 
   test("Date format of yyyy-MM-dd"){
+    new TransformDateData {
+      val trans = DateUtils.convertDateYearDash(leapSavingsDate3)
+        .select("CUST_ID", "TXN_ID", "TXN_AMT", "ITEM_QTY", "DISC_AMT", "Date")
 
+      assertDataFrameEquals(trans, dateResults)
+    }
   }
 
   test("Date format of yyyy/MM/dd"){
+    new TransformDateData {
+      val trans = DateUtils.convertDateYearSlash(leapSavingsDate4)
+        .select("CUST_ID", "TXN_ID", "TXN_AMT", "ITEM_QTY", "DISC_AMT", "Date")
 
+      assertDataFrameEquals(trans, dateResults)
+    }
   }
 
 }
