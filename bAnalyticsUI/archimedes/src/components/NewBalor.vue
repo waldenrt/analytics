dfd2fb68c9dd783ea49302fc87ad627fd782bae8
jfullylove <jfullylove@iMac-Fullylove.local>
2017-09-12 08:51:19 -0500

    styled and coded Balor Input Page & Balor Cadence Page. Recoded Cadence Summary table.

--- bAnalyticsUI/archimedes/src/components/NewBalor.vue
+++ bAnalyticsUI/archimedes/src/components/NewBalor.vue
@@ -1,15 +1,86 @@
 <template>
-  <div class="NewBalor">
-    <h1>{{ msg }}</h1>
-  </div>
+    <v-container fluid grid-list-md class="NewBalor">
+    <v-layout row wrap>
+      <!-- COLUMN1 -->
+      <v-flex d-flex xs12 sm6>
+        <!-- CARD1 -->
+        <v-card class="white">
+          <v-card-title primary-title class="primary">
+            <h6 class="white--text text-xs-left mb-0">Enter Job Name</h6>
+          </v-card-title>
+            <v-text-field
+              name="input-1"
+              label="Enter job name"
+              class="ma-3 pr-4"
+              required
+            ></v-text-field>
+        </v-card>
+        <!-- //CARD1 -->
+        <!-- CARD2 -->
+        <v-card class="white">
+          <v-card-title primary-title class="primary">
+            <h6 class="white--text text-xs-left mb-0">Select file for analysis</h6>
+          </v-card-title>
+          <v-select
+              v-model="dialog"
+              persistent
+              label="Select"
+              single-line
+              auto
+              prepend-icon="search"
+              slot="activator"
+              class="ma-3 pr-4"
+              required
+            ></v-select>
+        </v-card>
+        <!-- //CARD2 -->
+        <!-- CARD3 -->
+        <v-card class="white">
+          <v-card-title primary-title class="primary">
+            <h6 class="white--text text-xs-left mb-0">Select file type</h6>
+          </v-card-title>
+          <v-select
+              v-bind:items="items"
+              v-model="e1"
+              label="Select"
+              class="ma-3 pr-4"
+              single-line
+              bottom
+              v-bind:error-messages="['Please select an option']"
+              required
+            ></v-select>
+        </v-card>
+        <v-btn @click="submit" :class="{ green: valid, red: !valid }" class="primary white--text">submit</v-btn>
+        <v-btn @click="clear">clear</v-btn>
+        <!-- CARD2 -->
+      </v-flex>
+      <!-- //COLUMN1 -->
+      <!-- COLUMN2 -->
+      <v-flex d-flex xs12 sm6>
+        <v-card class="white">
+          <img src="http://via.placeholder.com/525x150/0087aa/ffffff?text=[Image+of+Sample+file+preview]" width="100%" height="100%" style="padding:10px;">
+        </v-card>
+        <v-card class="white mt-3">
+          <img src="http://via.placeholder.com/525x150/0087aa/ffffff?text=User+File+Preview" width="100%" height="100%" style="padding:10px;">
+        </v-card>
+      </v-flex>
+      <!-- //COLUMN2 -->
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
-    name: 'newBalor',
     data () {
       return {
-        msg: 'Create new BALOR job'
+        e1: null,
+        items: [
+          { text: '.txt (tab separated)' },
+            { text: '.CSV ("|" delimeter)' },
+          { text: '.DSV ("|" delimeter)' },
+          { text: '.DSV (";" delimeter)' }
+        ],
+        dialog: false
       }
     }
   }
