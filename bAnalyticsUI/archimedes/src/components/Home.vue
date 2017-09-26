03a299928ee7db067db4a27b8a7c0e266fa43f4e
jfullylove <jfullylove@iMac-Fullylove.local>
2017-09-26 15:24:56 -0500

    merging changes

--- bAnalyticsUI/archimedes/src/components/Home.vue
+++ bAnalyticsUI/archimedes/src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-parallax src="../assets/images/bp_logo_reverse_color.png" height="1500">
+  <v-parallax src="../assets/images/bp_logo_reverse_color.png" height="1400">
     <v-layout column align-center justify-center>
-      <h3 class="text-xs-center">Consumer Insights Dashboard</h3>
+      <h3 class="primary--text text-xs-center">Consumer Insights Dashboard</h3>
       <img src="../assets/images/bp_logo_reverse_color.png" />
     </v-layout>
   </v-parallax>
