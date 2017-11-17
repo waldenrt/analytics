5f492803edb27f1b2c4cbe300737311e8c791571
merrill-brier <amerrill@brierley.com>
2017-11-17 15:43:10 -0600

    inner doughnut text updates with chart

--- bAnalyticsUI/archimedes/src/components/lifecycleCharts/doughnutChart.js
+++ bAnalyticsUI/archimedes/src/components/lifecycleCharts/doughnutChart.js
@@ -20,27 +20,40 @@ export default Doughnut.extend({
     }
   },
   mounted () {
-    var text1 = this.chartData.datasets[0].data[0]
-    this.addPlugin({
-      id: 'centerText',
-      beforeDraw: function (chart) {
-        var width = chart.chart.width
-        var height = chart.chart.height
-        var ctx = chart.chart.ctx
-
-        ctx.restore()
-        var fontSize = (height / 114).toFixed(2)
-        ctx.font = fontSize + 'em sans-serif'
-        ctx.textBaseline = 'middle'
-
-        var text = text1.toFixed(2) + '%'
-        var textX = Math.round((width - ctx.measureText(text).width) / 2)
-        var textY = height / 2
-
-        ctx.fillText(text, textX, textY)
-        ctx.save()
-      }
-    })
+    this.createPlugin()
     this.renderChart(this.chartData, this.options)
+  },
+  methods: {
+    createPlugin () {
+      var text1 = this.chartData.datasets[0].data[0]
+      this.addPlugin({
+        id: 'centerText',
+        beforeDraw: function (chart) {
+          var width = chart.chart.width
+          var height = chart.chart.height
+          var ctx = chart.chart.ctx
+
+          ctx.restore()
+          var fontSize = (height / 114).toFixed(2)
+          ctx.font = fontSize + 'em sans-serif'
+          ctx.textBaseline = 'middle'
+
+          var text = text1.toFixed(2) + '%'
+          var textX = Math.round((width - ctx.measureText(text).width) / 2)
+          var textY = height / 2
+
+          ctx.clearRect(textX - 50, textY - 50, 100, 100)
+          ctx.fillText(text, textX, textY)
+          ctx.save()
+        }
+      })
+    }
+  },
+  watch: {
+    'chartData': function () {
+      this._chart.destroy()
+      this.createPlugin()
+      this.renderChart(this.chartData, this.options)
+    }
   }
 })
